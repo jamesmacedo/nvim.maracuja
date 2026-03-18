49c7a0a087877750a33b1527397a80656204636b
James Yan <jamesyancm@gmail.com>
2026-03-18 18:33:59 -0300

    fix: Implemented the movement system to jump across the marks

--- lua/maracuja/ui.lua
+++ lua/maracuja/ui.lua
@@ -2,7 +2,11 @@ local state = require("maracuja.models.state")
 local config = require("maracuja.config")
 local move = require("maracuja.logic.movement")
 
-local UI = {}
+local UI = {
+	lines = {},
+	ids = {},
+	current_pos = 1
+}
 
 local function setup_highlights()
 	vim.api.nvim_set_hl(0, "MyMenuBadge", { bg = "#2e3e43", fg = "#89ddff" })
@@ -10,21 +14,9 @@ local function setup_highlights()
 	vim.api.nvim_set_hl(0, "MyMenuFile", { fg = "#ebdbb2" })
 end
 
-function UI.show_window()
-	setup_highlights()
+function UI.draw()
 
-	local width = 40
-	local height = #state.orders
-
-	local lines = {}
-	local ids = {}
-
-	for pos, item in ipairs(state.orders) do
-		table.insert(lines, " " .. state.marks[item].line:sub(0, 30))
-		ids[pos] = state.marks[item].id
-	end
-
-	vim.api.nvim_buf_set_lines(state.buffer, 0, -1, false, lines)
+	vim.api.nvim_buf_clear_namespace(state.buffer, config.ui, 0, -1)
 
 	for i, item in ipairs(state.orders) do
 		local badge_hl = "MyMenuBadge"
@@ -38,8 +30,54 @@ function UI.show_window()
 			hl_mode = "combine",
 		})
 
-		vim.api.nvim_buf_add_highlight(state.buffer, config.ui, "MyMenuFile", i - 1, 0, -1)
+		if UI.current_pos == i then
+			vim.api.nvim_buf_add_highlight(state.buffer, config.ui, "MyMenuFile", i - 1, 0, -1)
+		end
 	end
+end
+
+function UI.move(step)
+	local new_pos = UI.current_pos + step
+	if (new_pos > 0 and new_pos <= #UI.lines) then
+		UI.current_pos = new_pos
+	end
+
+	vim.notify(tostring(UI.current_pos))
+	UI.draw()
+end
+
+function UI.show_window()
+	setup_highlights()
+
+	local original_guicursor = vim.opt.guicursor:get()
+
+	vim.api.nvim_set_hl(0, "HiddenCursor", { blend = 100, nocombine = true })
+
+	local width = 40
+	local height = #state.orders
+
+	for pos, item in ipairs(state.orders) do
+		table.insert(UI.lines, " " .. state.marks[item].line:sub(0, 30))
+		UI.ids[pos] = state.marks[item].id
+	end
+
+	-- UI.current_pos = #UI.lines
+
+	vim.api.nvim_buf_set_lines(state.buffer, 0, -1, false, UI.lines)
+
+	vim.api.nvim_create_autocmd("BufEnter", {
+		buffer = state.buffer,
+		callback = function ()
+			vim.opt.guicursor = "a:HiddenCursor"
+		end
+	})
+
+	vim.api.nvim_create_autocmd("BufLeave", {
+		buffer = state.buffer,
+		callback = function ()
+			vim.opt.guicursor = original_guicursor
+		end
+	})
 
 	UI.window = vim.api.nvim_open_win(state.buffer, true, {
 		relative = "cursor",
@@ -52,17 +90,22 @@ function UI.show_window()
 	})
 
 	vim.keymap.set("n", "<CR>", function()
-		local pos = vim.api.nvim_win_get_cursor(0)[1]
-
-		local m = state.marks[ids[pos]]
+		local m = state.marks[UI.ids[UI.current_pos]]
 
 		if m ~= nil then
+			vim.notify(m.id)
 			move.jump_to(m.id)
 		end
 
 		vim.api.nvim_win_close(UI.window, true)
 	end)
 
+	local opts = { buffer = state.buffer, noremap = true, silent = true }
+	vim.keymap.set("n", "<Up>", function() UI.move(-1) end, opts)
+	vim.keymap.set("n", "<Down>", function() UI.move(1) end, opts)
+
+
+	UI.draw()
 	-- vim.api.nvim_win_set_option(UI.window, "cursorline", true)
 	-- vim.api.nvim_win_set_option(UI.window, "winhl", "Normal:NormalFloat,CursorLine:MyMenuSelected")
 end
